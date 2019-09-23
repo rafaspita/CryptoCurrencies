167dca58264563870bc9c98e65703349f156266f
Rafael Spitaliere <rafael.spitaliere@gmail.com>
2019-09-23 11:23:36 -0300

    project and gradle initial configurations

---
+++ app/src/main/java/com/spitaliere/cryptocurrencies/CryptoCurrenciesApplication.kt
@@ -0,0 +1,11 @@
+package com.spitaliere.cryptocurrencies
+
+import android.app.Application
+
+/**
+ * Created by Rafael Spitaliere on 23/09/2019.
+ **/
+class CryptoCurrenciesApplication : Application() {
+
+
+}
